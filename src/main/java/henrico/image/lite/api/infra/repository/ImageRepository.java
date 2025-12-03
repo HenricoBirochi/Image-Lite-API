cb637550be355d0154a02b4349d48613a47f27a4
Henrico Birochi <henrico.birochi@gmail.com>
2025-12-03 08:01:28 -0300

    feat: implement image search functionality by extension and name or tags

--- src/main/java/henrico/image/lite/api/infra/repository/ImageRepository.java
+++ src/main/java/henrico/image/lite/api/infra/repository/ImageRepository.java
@@ -1,7 +1,47 @@
 package henrico.image.lite.api.infra.repository;
 
 import henrico.image.lite.api.domain.entity.Image;
+import henrico.image.lite.api.domain.enums.ImageExtension;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    /*
+    * @param extension
+    * @param query
+    *
+    * SELECT * FROM images WHERE 1 = 1 AND extension = 'PNG' AND ( name LIKE 'query' OR tags LIKE 'query' )
+    * */
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query) {
+        // SELECT * FROM images WHERE 1 = 1
+        Specification<Image> conjunction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
+        Specification<Image> spec = Specification.where(conjunction);
+
+        if(extension != null) {
+            // AND extension = 'PNG'
+            Specification<Image> extensionEqual = (root, q, cb) -> cb.equal(root.get("extension"), extension);
+            spec = spec.and(extensionEqual);
+        }
+
+        if(StringUtils.hasText(query)) {
+            // AND ( name LIKE 'query' OR tags LIKE 'query' )
+            // RIVER => %RI%
+            Specification<Image> nameLike = (root, q, cb)
+                    -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
+            Specification<Image> tagsLike = (root, q, cb)
+                    -> cb.like(cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%");
+
+            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
+
+            spec = spec.and(nameOrTagsLike);
+        }
+
+        return findAll();
+    }
 }
